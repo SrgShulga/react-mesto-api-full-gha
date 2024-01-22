1245885c54410585f2f647bb9a83b83836e46c0c
Sergei Shulga <k1ft4999@gmail.com>
2024-01-22 06:28:15 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsHandler = require('./middlewares/cors-handler');
 
 const app = express();
-app.use(cors());
+app.use((corsHandler()));
 
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
--- backend/middlewares/cors-handler.js
+++ backend/middlewares/cors-handler.js
@@ -1,22 +1,23 @@
-// const allowedList = [
-//   'http://localhost:3000',
-//   'https://mestosocial.students.nomoredomainsmonster.ru/'
-// ]
+const allowedList = [
+  'http://localhost:3000',
+  'https://mestosocial.students.nomoredomainsmonster.ru/',
+];
 
-// const corsHandler = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE' ;
-//   const requestHeaders = req. headers['access-control-request-headers'];
-//   res.header ('Access-Control-Allow-Credentials', true);
+const corsHandler = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
 
-//   if (allowedList.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method = 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders)
-//     return res.end();
-//   }
-//   return next();
-// };
+  if (allowedList.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+};
+module.exports = corsHandler;
